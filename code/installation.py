ada13e125c6184cd574396ab3d7f20b58c667366
Izran Shaikh <izran.s032@gmail.com>
2026-03-08 19:08:18 +0530

    self installation n command setter
    startup command function provided by Sonia

--- code/installation.py
+++ code/installation.py
@@ -1,10 +1,38 @@
-#### removal of all tools including python's httpx.
-#### installation of all tools.
-#### installation of python library requests.
-#### go bin path export in bashrc n zshrc.
-#### figure out that ghost bug of subfinder.
+#### self installing script.
+#### ghost bug fixed ig.
 
 import subprocess as sp
+import os
+
+def set_startup_command(command):
+    home = os.environ["HOME"]
+    config_files = [
+        os.path.join(home, ".bashrc"),
+        os.path.join(home, ".zshrc")
+    ]
+    marker_start = "# ENV_SETTINGS_START"
+    marker_end = "# ENV_SETTINGS_END"
+    for config in config_files:
+        if os.path.exists(config):
+            with open(config, "r") as f:
+                lines = f.readlines()
+            new_lines = []
+            inside_block = False
+            for line in lines:
+                if marker_start in line:
+                    inside_block = True
+                    continue
+                if marker_end in line:
+                    inside_block = False
+                    continue
+                if not inside_block:
+                    new_lines.append(line)
+            new_lines.append("\n" + marker_start + "\n")
+            new_lines.append(command + "\n")
+            new_lines.append(marker_end + "\n")
+            with open(config, "w") as f:
+                f.writelines(new_lines)
+    print(f"\n{str(command)} set successfully")
 
 TOOLS = [
     {
@@ -13,10 +41,20 @@ TOOLS = [
         "extra": ""
     },
     {
-        "name": "go",
-        "cmd": "sudo apt install golang-go -y",
-        "extra": r"""RC=~/."${0##*/}"rc; echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> "$RC" && . "$RC" """
+        "name": "pipx",
+        "cmd": "sudo apt install pipx -y",
+        "extra": "pipx ensurepath"
     },
+    {
+        "name": "postleaks",
+        "cmd": "pipx install postleaks",
+        "extra": ""
+    },
+    # {
+    #     "name": "go",
+    #     "cmd": "sudo apt install golang-go -y",
+    #     "extra": r"""RC=~/."${0##*/}"rc; echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> "$RC" && . "$RC" """
+    # },
     {
         "name": "dirsearch",
         "cmd": "sudo apt install dirsearch -y",
@@ -51,6 +89,11 @@ TOOLS = [
         "name": "sublist3r",
         "cmd": "sudo apt install sublist3r -y",
         "extra": ""
+    },
+    {
+        "name": "requests",
+        "cmd": "pip install requests",
+        "extra": "sudo apt install python3-requests -y"
     }
 ]
 
@@ -67,7 +110,8 @@ def installer(config):
 for t in TOOLS:
     installer(t)
 
+set_startup_command('export PATH=$PATH:$(go env GOPATH)/bin')
+set_startup_command('export PATH=$PATH:~/.local/bin')
+set_startup_command('export PATH=$PATH:$(pwd)')
 
-
-
-'''
+print("\n\n[+] All tools installed! Please restart your terminal to apply changes.")
